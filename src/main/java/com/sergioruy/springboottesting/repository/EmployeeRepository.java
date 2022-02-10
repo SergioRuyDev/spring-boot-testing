2651b04f91d211d17abbc41635379bd1b60d5d58
SergioRuyDev <sergioruyenator@gmail.com>
2022-02-09 21:08:43 -0300

    Repository test in progress. Method save tested.

--- src/main/java/com/sergioruy/springboottesting/repository/EmployeeRepository.java
+++ src/main/java/com/sergioruy/springboottesting/repository/EmployeeRepository.java
@@ -4,4 +4,6 @@ import com.sergioruy.springboottesting.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
 }
+
---
+++ src/test/java/com/sergioruy/springboottesting/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.sergioruy.springboottesting.repository;
+
+import com.sergioruy.springboottesting.model.Employee;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @DisplayName("Junit test for save Employee Operation")
+    @Test
+    public void givenEmployeeObject_whenSave_thenReturnedSavedEmployee() {
+
+        //given - is a precondition or a setup
+        Employee employee = Employee.builder()
+                .firstName("Sergio")
+                .lastName("Ruy")
+                .email("sergio@gmail.com")
+                .build();
+
+        //when - is the action or the behavior we are going to test
+        Employee employeeSaved = employeeRepository.save(employee);
+
+        //then - verify the result
+        assertThat(employeeSaved).isNotNull();
+        assertThat(employeeSaved.getId()).isGreaterThan(0);
+    }
+
+//    @DisplayName("Junit test for get all Employees Operation")
+//    @Test
+//    public void givenEmployeeObject_whenGetAllEmployees_thenReturnedAllEmployees() {
+//
+//        //given - is a precondition or a setup
+//        Employee employee = Employee.builder()
+//                .firstName("Sergio")
+//                .lastName("Ruy")
+//                .email("sergio@gmail.com")
+//                .build();
+//
+//        //when - is the action or the behavior we are going to test
+//        Employee employeeSaved = employeeRepository.findAll(employee);
+//
+//        //then - verify the result
+//        assertThat(employeeSaved).isNotNull();
+//        assertThat(employeeSaved.getId()).isGreaterThan(0);
+//    }
+
+}
