c788b9b43ad5a2826a8181423d66bf5b45d438a8
SergioRuyDev <sergioruyenator@gmail.com>
2022-02-09 21:32:27 -0300

    Repository test in progress. Method FindAll tested.

--- src/test/java/com/sergioruy/springboottesting/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/sergioruy/springboottesting/repository/EmployeeRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -34,23 +36,46 @@ class EmployeeRepositoryTest {
         assertThat(employeeSaved.getId()).isGreaterThan(0);
     }
 
-//    @DisplayName("Junit test for get all Employees Operation")
-//    @Test
-//    public void givenEmployeeObject_whenGetAllEmployees_thenReturnedAllEmployees() {
-//
-//        //given - is a precondition or a setup
-//        Employee employee = Employee.builder()
-//                .firstName("Sergio")
-//                .lastName("Ruy")
-//                .email("sergio@gmail.com")
-//                .build();
-//
-//        //when - is the action or the behavior we are going to test
-//        Employee employeeSaved = employeeRepository.findAll(employee);
-//
-//        //then - verify the result
-//        assertThat(employeeSaved).isNotNull();
-//        assertThat(employeeSaved.getId()).isGreaterThan(0);
-//    }
+//  JUnit test for get all employees operation
+    @Test
+    public void givenEmployeesList_whenFindAll_thenEmployeesList() {
+
+        //given - is a precondition or a setup
+        Employee employee1 = Employee.builder()
+                .firstName("Sergio")
+                .lastName("Ruy")
+                .email("sergio@gmail.com")
+                .build();
+
+        Employee employee2 = Employee.builder()
+                .firstName("John")
+                .lastName("Cena")
+                .email("john@gmail.com")
+                .build();
+
+        employeeRepository.save(employee1);
+        employeeRepository.save(employee2);
+
+        //when - is the action or the behavior we are going to test
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        //then - verify the result
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    //  Junit test for
+        @Test
+        public void given_when_then() {
+
+            //given - is a precondition or a setup
+
+            //when - is the action or the behavior we are going to test
+
+            //then - verify the result
+
+        }
+
+
 
 }
